929196acd0c2f0cafce5d9f3bfa260f5467fa404
mariacolindres <mcolindres06@unitec.edu>
2023-06-20 07:05:55 -0600

    prueba

---
+++ js/app.js
@@ -0,0 +1,92 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'RunGran Shop',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		},
+		{
+		path: '/info-camisa/',
+    	url: 'info-camisa.html',
+    	name: 'info-camisa',
+  		},
+		{
+			path: '/perfil/',
+			url: 'perfil.html',
+			name: 'perfil',
+			  }
+	],
+	dialog: {
+		title: 'RunGran Shop',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('¡ Welcome to Rungran Shop !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
